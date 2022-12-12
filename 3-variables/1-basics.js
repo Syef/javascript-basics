dbb4992f6a70b8406bd1ee7fc7bd072dfd33959b
Syed hafizullah <113756168+Syef@users.noreply.github.com>
2022-12-12 18:15:50 +0530

    basics data added

---
+++ 3-variables/1-basics.js
@@ -0,0 +1,40 @@
+// 1. Declaration
+// No value assigned to myName identifier (variable)
+var myName;
+
+// 2. Assigning value (any datatype) to myName identifier
+myName = "syed hafeez";
+
+//Deciaring variable and assigning value to it 
+var fullName = 'syed hafeez sameer';
+
+var firstName = 'syed';
+var middleName = 'hafeez';
+var lastName = 'sameer';
+
+var myFullName = firstName + middleName + lastName;
+
+var fullName = "firstName" + "middleName" + "lastName";
+
+//var myName = firstName + "  "+ middleName + " " + lastName;
+var myName = ${firstName} ${middleName}, ${lastName}
+
+var city = 'guntur';
+var state  + "ap";
+var country + "india";
+
+var address + `${city},${state},${country}`;
+
+// lowercase (small letter)x
+var x = 200 * 99;
+
+//uppercase (capital letters) X
+var X = 89 * 78 / 10;
+
+//'5' + 10 -> '510'
+//`${ '5' + 10}`
+
+var output = `${'5' + 10}`; //'510'
+var outputOne = `${Number('5') + 10}`;// '15'
+var outputTwo = `${+'5' + 10}`; //'15'
+
