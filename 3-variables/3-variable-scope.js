cfdc3c90be93b2b0cca192ed2122a31bcab8378d
Syed hafizullah <113756168+Syef@users.noreply.github.com>
2022-12-15 11:29:54 +0530

    let and cont file added

---
+++ 3-variables/3-variable-scope.js
@@ -0,0 +1,34 @@
+// create a blocked scope
+{
+    var abc = 100;
+    let xyz = 200;
+
+}
+
+//here we can asses 'adc' identifier value, but we can't have asses to 'xyz' identifier
+
+//don't get confuse with {} has ab object
+var myDetails = {
+    key: 'value',
+    name: 'syed',
+    age: 30,
+    designation: 'UI Devolaper'
+
+}
+{ 
+    var city = "guntur":
+    let designation = "UI devolaper";
+    }
+
+    {
+        var city = "ongole";
+        let Designation = "frontend Dev"
+
+    }
+
+    city // "ongole"
+    designation //"UI Dev"
+
+    console.log("syed")
+    console.log(23>78):
+    
