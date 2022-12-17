ce6290973d393bd43ae5dc6f20c485fc7b997dbc
syed hafeez <hafeezsyed786.hs@gmail.com>
2022-12-17 20:32:58 +0530

    new file added

---
+++ 5-input-and-output/table.js
@@ -0,0 +1,18 @@
+const readline = require("readline-sync");
+console.log("table generator");
+
+const table number = readline.question("enter number to generate to number table:");
+
+console.log("******************");
+console.log(`${table number} x 1 = ${table number*1}`);
+console.log(`${table number} x 2 = ${table number*2}`);
+console.log(`${table number} x 3 = ${table number*3}`);
+console.log(`${table number} x 4 = ${table number*4}`);
+console.log(`${table number} x 5 = ${table number*5}`);
+console.log(`${table number} x 6 = ${table number*6}`);
+console.log(`${table number} x 7 = ${table number*7}`);
+console.log(`${table number} x 8 = ${table number*8}`);
+console.log(`${table number} x 9 = ${table number*9}`);
+console.log(`${table number} x 10 = ${table number*10}`);
+console.log("******************");
+
