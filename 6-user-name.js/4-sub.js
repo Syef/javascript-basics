08a4678b44499a1aa198f40878a3a604e2e65d65
syed hafeez <hafeezsyed786.hs@gmail.com>
2022-12-19 18:28:04 +0530

    user-resume file added

---
+++ 6-user-name.js/4-sub.js
@@ -0,0 +1,11 @@
+const readline = require('readline-sync');
+
+console.log("Sum of Two Numbers");
+
+const a = +readline.question('please enter the value of a: ');
+const b = +readline.question('please enter the value of b: ');
+console.log(`value of ${a} is ${typeof a}`);
+console.log(`value of ${b} is ${typeof b}`);
+const sum = a-b;
+
+console.log(`sum of ${a} sub ${b} = ${sum}`);
